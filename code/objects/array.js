6d3faed2284b05e0964bdfcb275ce34ecbbf1266
Michele Nuzzi <michele.nuzzi.2014@gmail.com>
2022-03-31 23:10:39 +0200

    removed objects

--- code/objects/array.js
+++
@@ -1,5 +0,0 @@
-
-const myArr = [ 1,2,3 ];
-
-console.log( myArr[ 0 ]);
-console.log( myArr );
--- code/objects/object.js
+++
@@ -1,9 +0,0 @@
-
-const objVar = {
-    number: 42,
-    float: Math.PI
-};
-
-console.log( objVar );
-console.log( objVar.float );
-console.log( objVar[ "number" ] );
